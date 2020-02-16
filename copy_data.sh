f16059e4e4cdc84f40829ef09342f12e95bbaaa4
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2020-02-16 15:42:39 +0100

    Correct addons not properly being copied

--- copy_data.sh
+++ copy_data.sh
@@ -79,31 +79,44 @@ whiptail --defaultno --title "$TITLE" --yesno \
 \n$CTID_FROM (${CTID_FROM_HOSTNAME}) -> $CTID_TO (${CTID_TO_HOSTNAME})" 12 50 || exit
 info "Home Assistant data from '$CTID_FROM' to '$CTID_TO'"
 
+# Shutdown container if running
+if [ $(pct status $CTID_TO | sed 's/.* //') == 'running' ]; then
+  msg "Shutting down '$CTID_TO'..."
+  pct shutdown $CTID_TO
+fi
+
 # Mount container disks
 msg "Mounting container disks..."
+HA_PATH=/usr/share/hassio
 CTID_FROM_PATH=$(pct mount $CTID_FROM | sed -n "s/.*'\(.*\)'/\1/p") || \
-  die "There was a problem mounting the root disk of LXC '${CTID_FROM_PATH}'."
-[ -d "${CTID_FROM_PATH}/usr/share/hassio" ] || \
+  die "There was a problem mounting the root disk of LXC '${CTID_FROM}'."
+[ -d "${CTID_FROM_PATH}${HA_PATH}" ] || \
   die "Home Assistant directories in '$CTID_FROM' not found."
 CTID_TO_PATH=$(pct mount $CTID_TO | sed -n "s/.*'\(.*\)'/\1/p") || \
-  die "There was a problem mounting the root disk of LXC '${CTID_TO_PATH}'."
-[ -d "${CTID_TO_PATH}/usr/share/hassio" ] || \
+  die "There was a problem mounting the root disk of LXC '${CTID_TO}'."
+[ -d "${CTID_TO_PATH}${HA_PATH}" ] || \
   die "Home Assistant directories in '$CTID_TO' not found."
+
+# Remove destination container's data folders
+DOCKER_PATH=/var/lib/docker
+rm -rf ${CTID_TO_PATH}${HA_PATH}
+rm -rf ${CTID_TO_PATH}${DOCKER_PATH}
+
+# Increase destination container's rootfs size
+AVAILABLE_SPACE=$(df $CTID_TO_PATH | awk 'NR>1 {print $4}')
+REQUIRED_SPACE=$(du -sc ${CTID_FROM_PATH}{$HA_PATH,$DOCKER_PATH} | grep total | awk '{print $1}')
+INCREASE_SPACE="+$(($(echo $REQUIRED_SPACE 1.05 | awk '{printf "%4.0f\n",$1*$2}') - $AVAILABLE_SPACE))K"
+if [ $REQUIRED_SPACE -gt $AVAILABLE_SPACE ]; then
+  msg "Increasing rootdisk of '$CTID_TO' with ${INCREASE_SPACE}..."
+  pct unmount $CTID_TO
+  pct resize $CTID_TO rootfs ${INCREASE_SPACE} >/dev/null
+  pct mount $CTID_TO >/dev/null
+  warn "Review of container '$CTID_TO' disk size is encouraged to prevent running out of space."
+fi
 
 # Copy data between containers
 msg "Copying data between containers..."
-rm -rf ${CTID_TO_PATH}/usr/share/hassio
-cp -r ${CTID_FROM_PATH}/usr/share/hassio ${CTID_TO_PATH}/usr/share/hassio
-
-# Unmount container disks
-msg "Unmounting container disks..."
-pct unmount $CTID_FROM && unset CTID_FROM_PATH
-pct unmount $CTID_TO && unset CTID_TO_PATH
-
-# Reboot running LXC container
-if [ $(pct status $CTID_TO | sed 's/.* //') == 'running' ]; then
-  msg "Rebooting '$CTID_TO'..."
-  pct reboot $CTID_TO
-fi
+rsync -aqHSX ${CTID_FROM_PATH}${HA_PATH} $(dirname ${CTID_TO_PATH}${HA_PATH})
+rsync -aqHSX ${CTID_FROM_PATH}${DOCKER_PATH} $(dirname ${CTID_TO_PATH}${DOCKER_PATH})
 
 info "Successfully transferred data."
