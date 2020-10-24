fad821e2c3d2d0fb570f844379f03b247ff3b9c7
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2020-10-24 15:48:22 +0200

    Resemble 'supervised-installer' process
    Fixes #30

--- create_container.sh
+++ create_container.sh
@@ -128,7 +128,7 @@ PCT_OPTIONS=(
   -cmode shell
   -features nesting=1
   -hostname $HOSTNAME
-  -net0 name=eth0,bridge=vmbr0,ip=dhcp
+  -net0 name=eth0,bridge=vmbr0
   -onboot 1
   -ostype $OSTYPE
   -rootfs $ROOTFS,size=$DISK_SIZE
@@ -160,15 +160,113 @@ EOF
 # Setup container for Home Assistant
 msg "Starting LXC container..."
 pct start $CTID
-pct exec $CTID mkdir /setup
-for file in $(find setup/); do
-  if [[ $file == *.service ]]; then
-    PERMISSIONS=644
-  fi
-  pct push $CTID $file /setup/$(basename $file) -perms ${PERMISSIONS:-755}
-  unset PERMISSIONS
-done
-pct exec $CTID /setup/setup.sh
+
+### Begin LXC commands ###
+alias lxc-cmd="lxc-attach -n $CTID --"
+# Prepare container OS
+msg "Setting up container OS..."
+lxc-cmd dhclient -4
+lxc-cmd sed -i "/$LANG/ s/\(^# \)//" /etc/locale.gen
+lxc-cmd locale-gen >/dev/null
+lxc-cmd apt-get -y purge openssh-{client,server} >/dev/null
+
+# Update container OS
+msg "Updating container OS..."
+lxc-cmd apt-get update >/dev/null
+lxc-cmd apt-get -qqy upgrade &>/dev/null
+
+# Install prerequisites
+msg "Installing prerequisites..."
+lxc-cmd apt-get -qqy install \
+    avahi-daemon curl jq network-manager xterm &>/dev/null
+
+# Install Docker
+msg "Installing Docker..."
+lxc-cmd sh <(curl -sSL https://get.docker.com) &>/dev/null
+
+# Configure Docker configuration
+msg "Configuring Docker..."
+DOCKER_CONFIG_PATH='/etc/docker/daemon.json'
+HA_URL_BASE=https://github.com/home-assistant/supervised-installer/raw/master/files
+lxc-cmd mkdir -p $(dirname $DOCKER_CONFIG_PATH)
+lxc-cmd wget -qLO $DOCKER_CONFIG_PATH ${HA_URL_BASE}/docker_daemon.json
+lxc-cmd systemctl restart docker
+
+# Configure NetworkManager
+msg "Configuring NetworkManager..."
+NETWORKMANAGER_CONFIG_PATH='/etc/NetworkManager/NetworkManager.conf'
+lxc-cmd wget -qLO $NETWORKMANAGER_CONFIG_PATH ${HA_URL_BASE}/NetworkManager.conf
+lxc-cmd sed -i 's/type\:veth/interface-name\:veth\*/' $NETWORKMANAGER_CONFIG_PATH
+lxc-cmd dhclient -r &> /dev/null
+lxc-cmd systemctl restart NetworkManager
+lxc-cmd nm-online -q
+
+# Create Home Assistant config
+msg "Creating Home Assistant config..."
+HASSIO_CONFIG_PATH=/etc/hassio.json
+HASSIO_DOCKER=homeassistant/amd64-hassio-supervisor
+HASSIO_MACHINE=qemux86-64
+HASSIO_DATA_PATH=/usr/share/hassio
+lxc-cmd bash -c "cat > $HASSIO_CONFIG_PATH <<- EOF
+{
+    \"supervisor\": \"${HASSIO_DOCKER}\",
+    \"machine\": \"${HASSIO_MACHINE}\",
+    \"data\": \"${HASSIO_DATA_PATH}\"
+}
+EOF
+"
+
+# Pull Home Assistant Supervisor image
+msg "Downloading Home Assistant Supervisor container..."
+HASSIO_VERSION=$(lxc-cmd bash -c "curl -s https://version.home-assistant.io/stable.json | jq -e -r '.supervisor'")
+lxc-cmd docker pull "$HASSIO_DOCKER:$HASSIO_VERSION" > /dev/null
+lxc-cmd docker tag "$HASSIO_DOCKER:$HASSIO_VERSION" "$HASSIO_DOCKER:latest" > /dev/null
+
+# Install Home Assistant Supervisor
+msg "Installing Home Assistant Supervisor..."
+HASSIO_SUPERVISOR_PATH=/usr/sbin/hassio-supervisor
+HASSIO_SUPERVISOR_SERVICE=/etc/systemd/system/hassio-supervisor.service
+lxc-cmd wget -qLO $HASSIO_SUPERVISOR_PATH ${HA_URL_BASE}/hassio-supervisor
+lxc-cmd chmod a+x $HASSIO_SUPERVISOR_PATH
+lxc-cmd wget -qLO $HASSIO_SUPERVISOR_SERVICE ${HA_URL_BASE}/hassio-supervisor.service
+lxc-cmd sed -i "s,%%HASSIO_CONFIG%%,${HASSIO_CONFIG_PATH},g" $HASSIO_SUPERVISOR_PATH
+lxc-cmd sed -i -e "s,%%BINARY_DOCKER%%,/usr/bin/docker,g" \
+  -e "s,%%SERVICE_DOCKER%%,docker.service,g" \
+  -e "s,%%BINARY_HASSIO%%,${HASSIO_SUPERVISOR_PATH},g" \
+  $HASSIO_SUPERVISOR_SERVICE
+lxc-cmd systemctl enable hassio-supervisor.service > /dev/null 2>&1
+
+# Create service to fix Home Assistant boot time check
+msg "Creating service to fix boot time check..."
+pct push $CTID hassio-fix-btime.service /etc/systemd/system/hassio-fix-btime.service
+lxc-cmd mkdir -p ${HASSIO_SUPERVISOR_SERVICE}.wants
+lxc-cmd ln -s /etc/systemd/system/{hassio-fix-btime.service,hassio-supervisor.service.wants/}
+
+# Start Home Assistant Supervisor
+msg "Starting Home Assistant..."
+lxc-cmd systemctl start hassio-supervisor.service
+
+# Install 'ha' cli
+msg "Installing the 'ha' cli..."
+lxc-cmd wget -qLO /usr/bin/ha ${HA_URL_BASE}/ha
+lxc-cmd chmod a+x /usr/bin/ha
+
+# Setup 'ha' cli prompt
+msg "Configuring 'ha' cli prompt..."
+HA_CLI_PATH=/usr/sbin/hassio-cli
+lxc-cmd wget -qLO $HA_CLI_PATH https://github.com/home-assistant/operating-system/raw/dev/buildroot-external/rootfs-overlay/usr/sbin/hassos-cli
+lxc-cmd sed -i 's,/bin/ash,/bin/bash,g' $HA_CLI_PATH
+lxc-cmd sed -i 's,^\(mesg n.*\)$,# \1,' /root/.profile
+lxc-cmd chmod a+x $HA_CLI_PATH
+lxc-cmd usermod --shell $HA_CLI_PATH root
+lxc-cmd bash -c "echo -e '\ncd $HASSIO_DATA_PATH' >> /root/.bashrc"
+
+# Cleanup container
+msg "Cleanup..."
+lxc-cmd apt-get autoremove >/dev/null
+lxc-cmd apt-get autoclean >/dev/null
+lxc-cmd rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
+### Finish LXC commands ###
 
 # Get network details and show completion message
 IP=$(pct exec $CTID ip a s dev eth0 | sed -n '/inet / s/\// /p' | awk '{print $2}')
