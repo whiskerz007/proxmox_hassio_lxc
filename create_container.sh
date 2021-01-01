ea671eb86dfbef34eb513445da0c0f6210ed4b3f
whiskerz007 <2713522+whiskerz007@users.noreply.github.com>
2021-01-01 12:48:17 +0100

    Use script to create LXC

--- create_container.sh
+++ create_container.sh
@@ -50,92 +50,30 @@ function cleanup() {
 TEMP_DIR=$(mktemp -d)
 pushd $TEMP_DIR >/dev/null
 
+# Create LXC
+export CTID=$(pvesh get /cluster/nextid)
+export PCT_OSTYPE=debian
+export PCT_OSVERSION=10
+export PCT_DISK_SIZE=4
+export PCT_OPTIONS="
+  -cmode shell
+  -features nesting=1
+  -hostname homeassistant
+  -net0 name=eth0,bridge=vmbr0
+  -onboot 1
+  -tags homeassistant
+"
+bash -c "$(wget -qLO - https://github.com/whiskerz007/proxmox_lxc_create/raw/main/lxc_create.sh)" || exit
+
+# Detect storage pool type
+STORAGE_TYPE=$(pvesm status -storage $(pct config $CTID | grep rootfs | awk -F ":" '{print $2}') | awk 'NR>1 {print $2}')
+if [ "$STORAGE_TYPE" == "zfspool" ]; then
+  warn "Some addons may not work due to ZFS not supporting 'fallocate'."
+fi
+
 # Download setup script
 REPO="https://github.com/whiskerz007/proxmox_hassio_lxc"
 wget -qO - ${REPO}/tarball/master | tar -xz --strip-components=1
-
-# Select storage location
-while read -r line; do
-  TAG=$(echo $line | awk '{print $1}')
-  TYPE=$(echo $line | awk '{printf "%-10s", $2}')
-  FREE=$(echo $line | numfmt --field 4-6 --from-unit=K --to=iec --format %.2f | awk '{printf( "%9sB", $6)}')
-  ITEM="  Type: $TYPE Free: $FREE "
-  OFFSET=2
-  if [[ $((${#ITEM} + $OFFSET)) -gt ${MSG_MAX_LENGTH:-} ]]; then
-    MSG_MAX_LENGTH=$((${#ITEM} + $OFFSET))
-  fi
-  STORAGE_MENU+=( "$TAG" "$ITEM" "OFF" )
-done < <(pvesm status -content rootdir | awk 'NR>1')
-if [ $((${#STORAGE_MENU[@]}/3)) -eq 0 ]; then
-  warn "'Container' needs to be selected for at least one storage location."
-  die "Unable to detect valid storage location."
-elif [ $((${#STORAGE_MENU[@]}/3)) -eq 1 ]; then
-  STORAGE=${STORAGE_MENU[0]}
-else
-  while [ -z "${STORAGE:+x}" ]; do
-    STORAGE=$(whiptail --title "Storage Pools" --radiolist \
-    "Which storage pool you would like to use for the container?\n\n" \
-    16 $(($MSG_MAX_LENGTH + 23)) 6 \
-    "${STORAGE_MENU[@]}" 3>&1 1>&2 2>&3) || exit
-  done
-fi
-info "Using '$STORAGE' for storage location."
-
-# Get the next guest VM/LXC ID
-CTID=$(pvesh get /cluster/nextid)
-info "Container ID is $CTID."
-
-# Download latest Debian LXC template
-msg "Updating LXC template list..."
-pveam update >/dev/null
-msg "Downloading LXC template..."
-OSTYPE=debian
-OSVERSION=${OSTYPE}-10
-mapfile -t TEMPLATES < <(pveam available -section system | sed -n "s/.*\($OSVERSION.*\)/\1/p" | sort -t - -k 2 -V)
-TEMPLATE="${TEMPLATES[-1]}"
-pveam download local $TEMPLATE >/dev/null ||
-  die "A problem occured while downloading the LXC template."
-
-# Create variables for container disk
-STORAGE_TYPE=$(pvesm status -storage $STORAGE | awk 'NR>1 {print $2}')
-case $STORAGE_TYPE in
-  dir|nfs)
-    DISK_EXT=".raw"
-    DISK_REF="$CTID/"
-    ;;
-  zfspool)
-    DISK_PREFIX="subvol"
-    DISK_FORMAT="subvol"
-    ;;
-esac
-DISK=${DISK_PREFIX:-vm}-${CTID}-disk-0${DISK_EXT-}
-ROOTFS=${STORAGE}:${DISK_REF-}${DISK}
-
-# Create LXC
-msg "Creating LXC container..."
-DISK_SIZE=4G
-pvesm alloc $STORAGE $CTID $DISK $DISK_SIZE --format ${DISK_FORMAT:-raw} >/dev/null
-if [ "$STORAGE_TYPE" == "zfspool" ]; then
-  warn "Some addons may not work due to ZFS not supporting 'fallocate'."
-else
-  mkfs.ext4 $(pvesm path $ROOTFS) &>/dev/null
-fi
-ARCH=$(dpkg --print-architecture)
-HOSTNAME=homeassistant
-TEMPLATE_STRING="local:vztmpl/${TEMPLATE}"
-PCT_OPTIONS=(
-  -arch $ARCH
-  -cmode shell
-  -features nesting=1
-  -hostname $HOSTNAME
-  -net0 name=eth0,bridge=vmbr0
-  -onboot 1
-  -ostype $OSTYPE
-  -rootfs $ROOTFS,size=$DISK_SIZE
-  -storage $STORAGE
-  -tags homeassistant
-)
-pct create $CTID $TEMPLATE_STRING ${PCT_OPTIONS[@]} >/dev/null
 
 # Modify LXC permissions to support Docker
 LXC_CONFIG=/etc/pve/lxc/${CTID}.conf
